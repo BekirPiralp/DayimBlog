88a16a2c5c474e9707cc5a0a7b5db03001110d0b
Bekir PİRALP <bpir33@gmail.com>
2022-09-11 06:19:52 +0300

    Şİmdilik giriş ekranı tasarlıyorum lakin şu an için uyuyacağımz ;D

--- src/app/components/login-kisim/login-kisim.component.ts
+++ src/app/components/login-kisim/login-kisim.component.ts
@@ -13,7 +13,20 @@ export class LoginKisimComponent implements OnInit {
 
 
   constructor(bgService:BackgroundService) {
-    bgService.test() 
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/07/25/16/00/art-3561710_960_720.jpg";
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2016/11/22/23/03/hardwood-1851071_960_720.jpg"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2016/03/18/15/02/ufo-1265186_960_720.jpg"
+    //bgService.resimUrl="https://pixabay.com/get/ga3062b5d88ed5cb6432886964762914da8fe8aa8cce2e852289e26e8d58494989ce57856fae13ccdc4601ba1ae3f1815ddc9f66cc5fac540029deb9a10af9848417c5a1a1a340ae199e023d7b8106c0a_1920.jpg"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2017/08/15/08/23/stars-2643089_960_720.jpg"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/01/30/22/50/forest-3119826_960_720.jpg";//000
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/08/21/23/29/forest-3622519_960_720.jpg"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg"
+    //bgService.resimUrl="https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=5000"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/01/31/07/36/secret-3120483_960_720.jpg"
+    //bgService.resimUrl="https://cdn.pixabay.com/photo/2018/03/09/22/19/annual-rings-3212803_960_720.jpg"
+    bgService.resimUrl="https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_960_720.jpg"
+
+    bgService.ArkaplanResminiDeğiştir() 
     }
 
   ngOnInit(): void {
--- src/services/backgroundServices/background.service.ts
+++ src/services/backgroundServices/background.service.ts
@@ -11,11 +11,22 @@ export class BackgroundService {
     background:  var(--bg-ana);*/
   constructor(sanitizer: DomSanitizer) {
     this._santizer=sanitizer;
+
+    this.rnk="";
+    this.renkler= [new Renk(0,0,0,0.8),new Renk(0,0,0,0.6),new Renk(255,255,255,0.2),new Renk(255,255,255,0.2),new Renk(0,0,0,0.6),new Renk(0,0,0,0.8)];
+    this.renkler.forEach((val)=>{
+      this.rnk +=val.toString+",";
+    })
+
+    this.rnk=this.rnk.substring(0,this.rnk.length -1);
+    this.dereceSet=90;
+    this.resimUrl="https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=2000";
+    this._resim="";
+    this._background="";
   }
 
   _santizer;
-  renk = 'rgba(255, 255, 255, 0.200)';
-  renk2 = 'var(rgba(0, 0, 0, 0.500))';
+  private renkler:Renk[];
   private _derece:string='90deg';
   set dereceSet(derece:number){
     this._derece= derece.toString()+'deg';
@@ -23,21 +34,82 @@ export class BackgroundService {
   get dereceGet(){
     return this._derece;
   }
-  resimUrl="/src/assets/backgroundRihtim.jpg";
-  private _resim=`url("${this.resimUrl}")`;
-  private _rsm = "url('https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=2000')";
-  private _background:string="linear-gradient(90deg,var(--rnk1),var(--rnk2),var(--rnk2),var(--rnk1)),var(--test)";
+  resimUrl:string;//= document.baseURI+"/src/assets/backgroundRihtim.jpg";
+  private _resim;
+  private rnk:string;
+  private _background:string;
 
-  test(){
-    console.log(this._background)
-    try{
-      document.body.style.cssText+="--test:"+this._rsm;
-      document.body.style.cssText+="--rnk1: #ffffff9f";
-      document.body.style.cssText+="--rnk2: #0000009f"
-      document.body.style.cssText+="--tst:"+this._background;
-      document.body.style.background="var(--tst)";
-    }catch(err){
-      console.log(err);
+  ArkaplanResminiDeğiştir(){
+    this._resim=`url('${this.resimUrl}')`;
+    this._background=`linear-gradient(${this._derece},${this.rnk}),${this._resim} no-repeat `;
+      document.body.style.background=this._background
+      document.body.style.backgroundSize= "100% 100%"
+  }
+}
+
+export class Renk{
+  /**
+   *
+   */
+  constructor(...args: [kırmızı?: number, mavi?: number, yeşil?: number, opaklık?: number]) {
+    this.kırmızı=args[0]!
+    this.mavi=args[1]!
+    this.yeşil=args[2]!
+    this.opaklık=args[3]!
+  }
+
+
+  private _kırmızı:number=0;
+  private _mavi:number=0;
+  private _yeşil:number=0;
+  private _opaklık:number=1;
+
+  public set kırmızı(val:number){
+    this._kırmızı=this.sett(val);
+  }
+  public set mavi(val:number){
+    this._mavi=this.sett(val);
+  }
+  public set yeşil(val:number){
+    this._yeşil=this.sett(val);
+  }
+
+  public set opaklık(val:number){
+    if(0<=val && 1>=val )
+    this._opaklık=val;
+    else if(val<0)
+    this._opaklık=0;
+    else
+    this._opaklık=1;
+  }
+
+  public get kırmızı(){
+    return this._kırmızı;
+  }
+  
+  public get mavi(){
+    return this._mavi;
+  }
+  public get yeşil(){
+    return this._yeşil;
+  }
+
+  public get opaklık(){
+    return this._opaklık;
+  }
+
+  private sett(val:number){
+    if(val>0){
+      if(val>255)
+      return 255;
+      else
+      return val;
+    }else{
+      return 0;
     }
+  }
+
+  public get toString():string{
+    return `rgba(${this._kırmızı},${this._yeşil},${this._mavi},${this._opaklık})`;
   }
 }
