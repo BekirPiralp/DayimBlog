5f5785c5e0f8305a8115e66fd838e3c399d9b8cd
BekirPiralp <bpir33@gmail.com>
2022-10-02 22:19:44 +0300

    Temeller için şimdilik ayarlandı

--- src/services/localStroge/base/baseService.ts
+++ src/services/localStroge/base/baseService.ts
@@ -33,6 +33,26 @@ export class BaseKeyValHanler<ValueType ,KeyValue extends  IKeyVal<ValueType>> i
 
 }
 
+export class TokenVal extends KeyVal<string> implements ITokenVal {
+    override parseValType(Value: string | null): string {
+        return String(Value);
+    }
+}
+
+export class RefreshTokenVal extends KeyVal<string> implements IRefreshTokenVal{
+    override parseValType(Value: string | null): string {
+        return String(Value);
+    }
+}
+
+export class TokenValHandler extends BaseKeyValHanler<String,TokenVal> implements ITokenValHandler{
+
+}
+
+export class RefreshTokenValHandler extends BaseKeyValHanler<String,RefreshTokenVal> implements IRefreshTokenValHandler{
+
+}
+
 
 /**
  * abstract
@@ -44,8 +64,23 @@ export interface IKeyVal<ValType>{
     parseValType(Value:string | null):ValType;
 }
 
+export interface ITokenVal extends IKeyVal<String>{
+
+}
+
+export interface IRefreshTokenVal extends IKeyVal<String>{
+
+}
+
 export interface IBaseKeyValHanler<ValueType,KeyValue extends IKeyVal<ValueType>>{
     set(setObje:KeyValue):void;
     get():KeyValue;
 }
 
+export interface ITokenValHandler extends IBaseKeyValHanler<String,ITokenVal>{
+
+}
+
+export interface IRefreshTokenValHandler extends IBaseKeyValHanler<String,IRefreshTokenVal>{
+
+}
