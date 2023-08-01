af8722edd92d4409276f2d27eba3aba2361b03a6
BekirPiralp <bpir33@gmail.com>
2023-08-02 02:59:25 +0300

    özel taga veri yollama yapıldı

--- src/app/components/ozel/sosyal-medya-logo/sosyal-medya-logo.component.ts
+++ src/app/components/ozel/sosyal-medya-logo/sosyal-medya-logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input } from '@angular/core';
 
 @Component({
   selector: 'sosyal-medya-logo',
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SosyalMedyaLogoComponent implements OnInit {
 
+  private _instagramURL:string | undefined ="#";
+  private _twitterURL:string | undefined ="#";
+  private _facebookURL:string | undefined ="#";
   constructor() { }
 
   ngOnInit(): void {
+    //console.log(this.asd);
+  }
+  
+  @Input() set instagramUrl(url : string | undefined) {
+    console.log(url);
+    if(url == undefined)
+      url="#";
+    this._instagramURL = url;
   }
 
+  @Input() set twitterUrl (url:string | undefined){
+    if(url == undefined)
+      url="#";
+    this._twitterURL = url;
+  }
+
+  @Input() set facebookUrl (url:string | undefined){
+    if(url == undefined)
+      url="#";
+    this._facebookURL = url;
+  }
+  
+  public get instagramUrl(): string {
+
+    return this._instagramURL!;
+  }
+
+  public get twitterUrl ():string{
+    return this._twitterURL!;
+  }
+
+  public get facebookUrl (): string{
+    
+    return this._facebookURL!;
+  }
+  
 }
