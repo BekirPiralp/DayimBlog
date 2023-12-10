1889b67b531378cf53dda361c025a86104f8b80f
BekirPiralp <bpir33@gmail.com>
2023-12-10 14:49:58 +0300

    yenikilikler

--- src/app/components/ozel/icerik-sayfa/icerik-sayfa.component.ts
+++ src/app/components/ozel/icerik-sayfa/icerik-sayfa.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { BackgroundService } from 'src/services/backgroundServices/background.service';
 
 @Component({
   selector: 'app-icerik-sayfa',
   templateUrl: './icerik-sayfa.component.html',
-  styleUrls: ['./icerik-sayfa.component.css']
+  styleUrls: ['./icerik-sayfa.component.css'],
+  providers:[BackgroundService]
 })
 export class IcerikSayfaComponent {
 
+  private _bgservice:BackgroundService;
+
+  constructor(bgService:BackgroundService){
+    this._bgservice=bgService;
+
+    this.arkaplan();
+  }
+
+  arkaplan(){
+    this._bgservice.resimUrl='../../../assets/backgroundSpace2.jpg';
+
+    this._bgservice.size="cover";
+
+    this._bgservice.ArkaplanResminiDeğiştir();
+  }
 }
